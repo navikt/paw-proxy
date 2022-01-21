63beefdd4966fceabc9d498458e989b52f51343e
Jørgen Tu Sveli <jsveli@gmail.com>
2022-01-21 15:28:07 +0100

    Legger inn deserialisering på http-klienten

--- src/main/kotlin/no/nav/pawproxy/app/HttpClientBuilder.kt
+++ src/main/kotlin/no/nav/pawproxy/app/HttpClientBuilder.kt
@@ -1,15 +1,26 @@
 package no.nav.pawproxy.app
 
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.databind.DeserializationFeature
 import io.ktor.client.*
 import io.ktor.client.engine.*
 import io.ktor.client.engine.apache.*
 import io.ktor.client.features.*
+import io.ktor.client.features.json.*
 
 object HttpClientBuilder {
 
     fun build(): HttpClient {
         return HttpClient(Apache) {
             install(HttpTimeout)
+
+            install(JsonFeature) {
+                serializer = JacksonSerializer {
+                    configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                    setSerializationInclusion(JsonInclude.Include.NON_NULL)
+                }
+            }
+
             engine { this.proxy = ProxyBuilder.http(requireProperty("HTTP_PROXY")) }
         }
     }

9785d1cf21c73355e6b47aa376e29e4999536236
Jørgen Tu Sveli <jsveli@gmail.com>
2022-01-21 14:44:36 +0100

    Proxy må eksplisitt settes i ktor http klient

--- src/main/kotlin/no/nav/pawproxy/app/HttpClientBuilder.kt
+++ src/main/kotlin/no/nav/pawproxy/app/HttpClientBuilder.kt
@@ -1,6 +1,7 @@
 package no.nav.pawproxy.app
 
 import io.ktor.client.*
+import io.ktor.client.engine.*
 import io.ktor.client.engine.apache.*
 import io.ktor.client.features.*
 
@@ -9,6 +10,7 @@ object HttpClientBuilder {
     fun build(): HttpClient {
         return HttpClient(Apache) {
             install(HttpTimeout)
+            engine { this.proxy = ProxyBuilder.http(requireProperty("HTTP_PROXY")) }
         }
     }
 

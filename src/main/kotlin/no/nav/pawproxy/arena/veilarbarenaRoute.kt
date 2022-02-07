b4db98a14b01698b61899ebe834fa265d1f83a03
Martine Enger <marenger@student.matnat.uio.no>
2022-02-07 12:24:37 +0100

    Forsøk på feilhåndtering på kall til veilarbarena slik at vi responderer med 404 (og ikke 500) når vi får 404 fra veilarbarena

--- src/main/kotlin/no/nav/pawproxy/arena/veilarbarenaRoute.kt
+++ src/main/kotlin/no/nav/pawproxy/arena/veilarbarenaRoute.kt
@@ -2,12 +2,13 @@ package no.nav.pawproxy.arena
 
 import io.ktor.application.*
 import io.ktor.client.*
+import io.ktor.client.features.*
 import io.ktor.client.request.*
+import io.ktor.http.*
 import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import no.nav.pawproxy.app.get
-import no.nav.pawproxy.app.isDevelopment
 import no.nav.pawproxy.app.logger
 import no.nav.pawproxy.oauth2.TokenService
 import no.nav.pawproxy.oauth2.veilarbarena
@@ -23,13 +24,28 @@ fun Route.veilarbarena(httpClient: HttpClient, tokenService: TokenService) {
             val path = call.request.uri
 
             val accessToken: String = tokenService.getAccessToken(call, veilarbarena)
-
-            val response = httpClient.get<String>("$veilarbarenaBaseUrl$path") {
-                header("Authorization", "Bearer $accessToken")
-                header("Nav-Consumer-Id", "veilarbregistrering")
-            }
-            logger.info("Respons fra veilarbarena med path $path: $response")
-            call.respondText(response)
+            Result.runCatching {
+                httpClient.get<String>("$veilarbarenaBaseUrl$path") {
+                    header("Authorization", "Bearer $accessToken")
+                    header("Nav-Consumer-Id", "veilarbregistrering")
+                }
+            }.fold(
+                onSuccess = {
+                    call.respondText(it)
+                        .also { response -> logger.info("Respons fra veilarbarena med path $path: $response") }
+                },
+                onFailure = {
+                    call.respond(exceptionToStatusCode(it), it.message ?: "Uventet feil")
+                }
+            )
         }
     }
 }
+
+fun exceptionToStatusCode(e: Throwable): HttpStatusCode =
+    when (e) {
+        is RedirectResponseException -> e.response.status
+        is ClientRequestException -> e.response.status
+        is ServerResponseException -> e.response.status
+        else -> HttpStatusCode.InternalServerError
+    }

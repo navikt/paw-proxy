d29ab60cac7c2688e697c5088f31a200288c7b32
Terje Heen <terje.heen@nav.no>
2022-09-12 15:50:36 +0200

    Legger til GET-route for abac
    Ping/helsesjekke benytter et GET-kall i bakgrunnen. Vi trenger derfor også å støtte GET.

--- src/main/kotlin/no/nav/pawproxy/abac/abacRoute.kt
+++ src/main/kotlin/no/nav/pawproxy/abac/abacRoute.kt
@@ -9,23 +9,50 @@ import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import no.nav.pawproxy.app.logger
 import no.nav.pawproxy.app.requireClusterName
 import no.nav.pawproxy.app.requireProperty
+import no.nav.pawproxy.http.forwardGet
 import no.nav.pawproxy.http.forwardPost
 import no.nav.pawproxy.http.handleExceptionAndRespond
 
 
 fun Route.abacRoute(httpClient: HttpClient) {
 
-    route("/abac{...}") {
+    route("/abac") {
         val abacUrl = requireProperty("ABAC_URL")
+
+        get {
+            if (requireClusterName().contains("prod")) {
+                throw IllegalStateException("Proxy mot ABAC kun tilgjengelig i dev")
+            }
+
+            logger.info("Har nådd get-endepunktet i abac-route")
+
+            val authHeader = call.request.header("Authorization") ?: call.respond(status = HttpStatusCode.Unauthorized, message = "GET-kall til /abac uten Authorization-header")
+
+            Result.runCatching {
+                httpClient.forwardGet<HttpResponse>(abacUrl) {
+                    header("Authorization", authHeader)
+                }
+            }.fold(
+                onSuccess = {
+                    call.respondBytes(status = it.status, bytes = it.readBytes())
+                },
+                onFailure = {
+                    call.handleExceptionAndRespond(it, "ABAC", abacUrl)
+                }
+            )
+        }
 
         post {
             if (requireClusterName().contains("prod")) {
                 throw IllegalStateException("Proxy mot ABAC kun tilgjengelig i dev")
             }
 
-            val authHeader = call.request.header("Authorization") ?: call.respond(status = HttpStatusCode.Unauthorized, message = "Kall til /abac uten Authorization-header")
+            logger.info("Har nådd post-endepunktet i abac-route")
+
+            val authHeader = call.request.header("Authorization") ?: call.respond(status = HttpStatusCode.Unauthorized, message = "POST-Kall til /abac uten Authorization-header")
             val body = call.receive<JsonNode>()
 
             Result.runCatching {
